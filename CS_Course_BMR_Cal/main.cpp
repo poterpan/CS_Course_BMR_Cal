0965e2d5d26d3bbe3f211272993990ed2217b46f
PoterPan <poterpan5466@gmail.com>
2021-12-08 11:51:02 +0800

    Initial Commit

---
+++ CS_Course_BMR_Cal/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS_Course_BMR_Cal
+//
+//  Created by Poter Pan on 2021/12/8.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
